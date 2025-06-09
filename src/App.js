793dd6f263571bce7f9b588f5f0d6d93b8fef20e
dpnsfounder <tatarmarian39@gmail.com>
2025-06-09 13:49:03 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>DPNS Buy DApp</h1>
+      <p>Welcome! Connect your wallet to begin.</p>
+    </div>
+  );
+}
+
+export default App;
