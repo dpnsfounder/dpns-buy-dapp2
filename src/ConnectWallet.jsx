d4400b4391b7f276b7f1cbdf241ffedfe2a59a6a
dpnsfounder <tatarmarian39@gmail.com>
2025-06-09 15:17:20 +0300

    Create ConnectWallet.jsx

---
+++ src/ConnectWallet.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
+const ConnectWallet = () => {
+  return (
+    <div style={{ margin: '20px' }}>
+      <ConnectButton />
+    </div>
+  );
+};
+
+export default ConnectWallet;
