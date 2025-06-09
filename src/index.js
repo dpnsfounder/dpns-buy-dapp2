dacab0494696fc71a8f04431681461d97e8e19d7
dpnsfounder <tatarmarian39@gmail.com>
2025-06-09 15:18:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,43 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.js";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import {
+  configureChains,
+  createConfig,
+  WagmiConfig,
+} from 'wagmi';
+import { bsc } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, publicClient } = configureChains(
+  [bsc],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'DPNS Buy DApp',
+  projectId: 'dpns-buy',
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  </React.StrictMode>
+);
